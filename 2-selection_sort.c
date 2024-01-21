8a55700b7455def91d9953250730375e7fce41a9
dArk10R4 <goylerov04@gmail.com>
2024-01-21 05:20:05 -0800

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,15 +21,20 @@ void swap(int *array, size_t i, size_t j)
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, min;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = i; j < size; j++)
+		min = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
-				swap(array, i, j);
+			if (array[min] > array[j])
+				min = j;
 		}
+
+		if (min != i)
+			swap(array, i, min);
+
 		print_array(array, size);
 	}
 }
