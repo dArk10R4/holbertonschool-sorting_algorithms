3c4e8d169ba0c008762e8628222fdb7dbf0fd531
dArk10R4 <goylerov04@gmail.com>
2024-01-21 05:28:42 -0800

    2

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
-#include <stdio.h>
 #ifndef SORT_H
 #define SORT_H
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -10,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
